081dc8f61fdf3f4df29286d22ae500135f88244c
Ebeid Soliman ElSayed <Ebeid@gatech.edu>
2016-07-31 21:44:34 -0700

    Course 1 :: Week 2 :: Assignment 1 :: Problem 1

---
+++ Assignment12/Assignment2_MaxPairwiseProduct/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Assignment2_MaxPairwiseProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arg1 = Console.ReadLine();
+            int n = 0;
+            if (int.TryParse(arg1, out n))
+                n = int.Parse(arg1);
+
+            string arg2 = Console.ReadLine();
+            Double[] nums = null;
+            if ((!string.IsNullOrEmpty(arg2)) && (null != arg2.Split(" ".ToCharArray())))
+            {
+                int temp = arg2.Split(" ".ToCharArray()).Length;
+                nums = new Double[temp];
+                int i = 0;
+                foreach (string str in arg2.Split(" ".ToCharArray()))
+                {
+                    nums[i] = Double.Parse(str);
+                    i++;
+                }
+            }
+            if ((n == 2) && (nums[0] != 0) && (nums[1] != 0))
+            {
+                Console.WriteLine(nums[0] * nums[1]);
+                return;
+            }
+            Double Maximum = 0;
+            int iM = 0;
+            Double secondMaximum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (Maximum < nums[i])
+                {
+                    Maximum = nums[i];
+                    iM = i;
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((i != iM)&& (secondMaximum < nums[i]))
+                {
+                    secondMaximum = nums[i];
+                }
+            }
+            //Console.WriteLine(Maximum);
+            //Console.WriteLine(secondMaximum);
+            Console.WriteLine(Maximum * secondMaximum);
+            //Console.ReadLine();
+        }
+    }
+}
