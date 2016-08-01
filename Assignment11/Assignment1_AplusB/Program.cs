abd2d6c2b8bed377d2c3e81a6038917ac7d726e9
Ebeid Soliman ElSayed <Ebeid@gatech.edu>
2016-07-31 21:42:45 -0700

    Course 1 :: Week 1 :: Assignment 1 :: Problem 1

---
+++ Assignment11/Assignment1_AplusB/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assignment1_AplusB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int result=0;
+            int temp;
+            foreach (string num in input.Split(" ".ToCharArray()))  
+            {
+                if (int.TryParse(num, out temp))
+                {
+                    result += temp;
+                }                        
+            }
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
